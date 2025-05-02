3ae063848a3278cf8af43c34420a0e845f75e4be
Vishnu Kumar <viku3999@colorado.edu>
2025-05-01 20:50:52 -0600

    Code clean up in main file

--- main.cpp
+++ main.cpp
@@ -72,49 +72,7 @@ void handle_sigint(int sig) {
     exit_flag = 1; // Set exit flag to indicate termination
 }
 
-// Toggle GPIO 17
-void GPIO_Toggle(){
-    static int init = 0;
-    if(!init){
-        GPIO_Init(17, GPIO_OUT);
-        init = 1;
-    }
-    static int gpio_value = 0;
-    if(gpio_value == 0) {
-        gpio_value = 1;
-        GPIO_Set_Value(17, GPIO_HIGH);
-        syslog(LOG_CRIT, "GPIO Task 4: GPIO17 value set to 1\n");
-    } 
-    else {
-        gpio_value = 0;
-        GPIO_Set_Value(17, GPIO_LOW);
-        syslog(LOG_CRIT, "GPIO Task 4: GPIO17 value set to 0\n");
-    }
-}
-
-// Toggle GPIO 17 based on GPIO 27 value
-void GPIO_Toggle_IP(){
-    static int init = 0;
-    if(!init){
-        GPIO_Init(17, GPIO_OUT);
-        GPIO_Init(27, GPIO_IN);
-        init = 1;
-    }
-    int gpio_value = 0;
-    GPIO_Get_Value(27, &gpio_value);
-    if(gpio_value == 0) {
-        gpio_value = 1;
-        GPIO_Set_Value(17, GPIO_HIGH);
-        syslog(LOG_CRIT, "GPIO Task 7: GPIO17 value set to 1\n");
-    } 
-    else {
-        gpio_value = 0;
-        GPIO_Set_Value(17, GPIO_LOW);
-        syslog(LOG_CRIT, "GPIO Task 7: GPIO17 value set to 0\n");
-    }
-}
-
-void GPIO_CHECKS(){
+void GPIO_Check(){
     static int init = 0;
     std::cout << "\n\tGPIO_CHECKS" << std::endl;
     if(!init){
@@ -145,78 +103,45 @@ void GPIO_CHECKS(){
     std::cout << "BUTTON_PIN_F4 value: " << gpio_value << std::endl;
 }
 
-void OLED_Check(){
-    static int fd, init = 0;
-    if(!init){
-        fd = i2c_init();
-        if (fd < 0) {
-            fprintf(stderr, "Failed to initialize I2C\n");
-        }
+void OLED_Check() {
+    int fd = i2c_init();
+    if (fd < 0) {
+        fprintf(stderr, "Failed to initialize I2C\n");
+    }
+
+    if (!SSD1106_init(fd)) {
+        fprintf(stderr, "Failed to initialize OLED\n");
+        i2c_close(fd);
+    }
+
+    // Display test pattern
+    // SSD1106_clear_screen(fd);
+    SSD1106_gotoXY(0, 0);
+    SSD1106_puts(fd, "RPi OLED Test", &Font_7x10, SSD1106_COLOR_WHITE);
     
-        if (!SSD1106_init(fd)) {
-            fprintf(stderr, "Failed to initialize OLED\n");
-            i2c_close(fd);
-        }
-        init = 1;
-    }
+    SSD1106_gotoXY(0, 16);
+    SSD1106_puts(fd, "7x10 Font Demo", &Font_7x10, SSD1106_COLOR_WHITE);
+    
+    SSD1106_gotoXY(0, 32);
+    SSD1106_puts(fd, "ABCDEFGHIJKLM", &Font_7x10, SSD1106_COLOR_WHITE);
+    
+    SSD1106_gotoXY(0, 48);
+    SSD1106_puts(fd, "NOPQRSTUVWXYZ", &Font_7x10, SSD1106_COLOR_WHITE);
+    
+    SSD1106_gotoXY(11, 48);
+    SSD1106_printDigit(fd, 3, &Font_7x10, SSD1106_COLOR_WHITE);
 
-    while(true){
-
-        for(int i = 0; i < 100; i++) {
-            for(int j = 0; j < 55; j++) {
-                // std::cout << i << " , "<< j << std::endl;
-                SSD1106_gotoXY(i, j);
-                SSD1106_puts(fd, ".", &Font_7x10, SSD1106_COLOR_WHITE);
-                SSD1106_update_screen(fd);
-                usleep(100);
-            }
-        }
-    }
+    SSD1106_update_screen(fd);
+    
+    // Clear display before exiting
+    // SSD1106_clear_screen(fd);
+    i2c_close(fd);
 }
 
-// void OLED_Check() {
-//     int fd = i2c_init();
-//     if (fd < 0) {
-//         fprintf(stderr, "Failed to initialize I2C\n");
-//     }
-
-//     if (!SSD1106_init(fd)) {
-//         fprintf(stderr, "Failed to initialize OLED\n");
-//         i2c_close(fd);
-//     }
-
-//     // Display test pattern
-//     // SSD1106_clear_screen(fd);
-//     SSD1106_gotoXY(0, 0);
-//     SSD1106_puts(fd, "RPi OLED Test", &Font_7x10, SSD1106_COLOR_WHITE);
-    
-//     SSD1106_gotoXY(0, 16);
-//     SSD1106_puts(fd, "7x10 Font Demo", &Font_7x10, SSD1106_COLOR_WHITE);
-    
-//     SSD1106_gotoXY(0, 32);
-//     SSD1106_puts(fd, "ABCDEFGHIJKLM", &Font_7x10, SSD1106_COLOR_WHITE);
-    
-//     SSD1106_gotoXY(0, 48);
-//     SSD1106_puts(fd, "NOPQRSTUVWXYZ", &Font_7x10, SSD1106_COLOR_WHITE);
-    
-//     SSD1106_gotoXY(11, 48);
-//     SSD1106_printDigit(fd, 3, &Font_7x10, SSD1106_COLOR_WHITE);
-
-//     SSD1106_update_screen(fd);
-    
-//     // Clear display before exiting
-//     // SSD1106_clear_screen(fd);
-//     i2c_close(fd);
-// }
-
-void Keypad_Test() {
+void Keypad_Check() {
     initialize_keypad();
 
-    // char key = detect_key_press();
     int key_num = get_key_press();
-    // if (key) {
-    //     printf("Pressed: %c\n", key);
-    // }
 
     if(key_num>-1){
         std::cout << "Key number: " << key_num << std::endl;
@@ -292,12 +217,7 @@ int get_keypad_button_floor(){
         init = 1;
     }
 
-    // int key_num = get_key_press();
-    // bcm2835_close();
-
     return get_key_press();
-
-    // return -1;
 }
 
 /* Controls the elevator door
@@ -325,7 +245,6 @@ void Service_1(){
         case ELEVATOR_DOOR_CLOSE:
             if (elevator_cpy.door_open_flag == 1) {
                 // Open the door
-                syslog(LOG_CRIT, "Service 1: Door is opening\n");
                 elevator_cpy.door_status = ELEVATOR_DOOR_OPENING;
                 elevator = elevator_cpy; // Update the shared elevator structure
             }
@@ -337,7 +256,6 @@ void Service_1(){
                 GPIO_Get_Value(IR_DOOR_PIN, &temp);
                 if (temp == IR_NOT_DETECTED) {
                     // Door is opened
-                    syslog(LOG_CRIT, "Service 1: Door is opened\n");
                     elevator_cpy.door_status = ELEVATOR_DOOR_OPEN;
                     elevator_cpy.door_open_flag = 0; // Reset the door open flag
                     tick_count = ELEVATOR_DOOR_OPEN_TIME; // Set the timer for door open time
@@ -350,7 +268,6 @@ void Service_1(){
             tick_count--;
             if (tick_count <= 0) {
                 // Close the door
-                syslog(LOG_CRIT, "Service 1: Door is closing\n");
                 elevator_cpy.door_status = ELEVATOR_DOOR_CLOSING;
                 elevator = elevator_cpy; // Update the shared elevator structure
             } 
@@ -360,7 +277,6 @@ void Service_1(){
                 GPIO_Get_Value(IR_OBSTACLE_PIN, &obstacle_value);
                 if (obstacle_value == IR_DETECTED) {
                     // Obstacle detected
-                    syslog(LOG_CRIT, "Service 1: Obstacle detected. Door will remain open\n");
                     tick_count = ELEVATOR_DOOR_OBSTACLE_TIME; // Set the timer for door open time
                     elevator = elevator_cpy; // Update the shared elevator structure
                 }
@@ -373,7 +289,6 @@ void Service_1(){
                 GPIO_Get_Value(IR_DOOR_PIN, &temp);
                 if (temp == IR_DETECTED) {
                     // Door is closed
-                    syslog(LOG_CRIT, "Service 1: Door is closed\n");
                     elevator_cpy.door_status = ELEVATOR_DOOR_CLOSE;
                     elevator = elevator_cpy; // Update the shared elevator structure
                 }
@@ -381,7 +296,6 @@ void Service_1(){
                 GPIO_Get_Value(IR_OBSTACLE_PIN, &temp);
                 if(temp == IR_DETECTED) {
                     // Obstacle detected
-                    syslog(LOG_CRIT, "Service 1: Obstacle detected. Door will open again\n");
                     elevator_cpy.door_status = ELEVATOR_DOOR_OPEN;
                     tick_count = ELEVATOR_DOOR_OBSTACLE_TIME; // Set the timer for door open time
 
@@ -419,13 +333,6 @@ void Service_2(){
 elevator_cpy2 = elevator; // Copy the shared elevator structure
 // mutex unlock 
 
-// std::cout<<"\nHI\n";
-// std::cout<<"Door status: "<<elevator_cpy2.door_status<<std::endl;
-// std::cout<<"Current floor: "<<elevator_cpy2.curr_floor<<std::endl;
-// std::cout<<"Destination floor: "<<elevator_cpy2.dest_floor<<std::endl;
-// std::cout<<"Floor status: "<<elevator_cpy2.floor_status<<std::endl;
-// std::cout<<"Door open flag: "<<elevator_cpy2.door_open_flag<<std::endl;
-// std::cout<<"Tick count: "<<tick_count<<std::endl;
 // If the door is closed, floor can be selected
     if(elevator_cpy2.door_status == ELEVATOR_DOOR_CLOSE) {
         int elevator_button_detected;
@@ -548,7 +455,9 @@ int main(int argc, char* argv[]) {
     syslog(LOG_CRIT, "RTES Course Project\nDone by: Sriya Garde, and Vishnu Kumar\n");
 
     if (argc < 2) {
-        std::cerr << "Usage: " << argv[0] << " -f <flag_value=1-3>" << std::endl;
+        std::cerr << "Usage: " << argv[0] << " -f <flag_value=1-4>" << std::endl;
+        std::cerr <<" 1 - Elevator services\n 2 - GPIO Check\n 3 - OLED Check\n 4 - Keypad Check" << std::endl;
+        std::cerr << "Please provide a flag value to run the appropriate service." << std::endl;
         return 1;
     }
 
@@ -560,33 +469,30 @@ int main(int argc, char* argv[]) {
             ++i; // Skip the value
         }
     }
+
     std::cout << "Flag value: " << flag_value << std::endl;
+    
     // Add services based on the flag value
     switch(flag_value) {
-        case 0: // Elevator services
+        case 1: // Elevator services
             sequencer.addService(&Service_1, 1, 99, 25);
             sequencer.addService(&Service_2, 1, 98, 50);
             sequencer.addService(&Service_3, 2, 99, 100);
             break;
-        case 1:
-            sequencer.addService(&GPIO_Toggle, 1, 98, 100);
-            break;
         case 2:
-            sequencer.addService(&GPIO_Toggle_IP, 1, 98, 100);
+            sequencer.addService(&GPIO_Check, 1, 99, 100);
+            break;
             break;
         case 3:
-            sequencer.addService(&GPIO_CHECKS, 1, 98, 100);
-            break;
+            sequencer.addService(&OLED_Check, 1, 99, 1000);
             break;
         case 4:
-            sequencer.addService(&OLED_Check, 1, 98, 1000);
-            break;
-            break;
-        case 5:
-            sequencer.addService(&Keypad_Test, 1, 98, 100);
+            sequencer.addService(&Keypad_Check, 1, 99, 100);
             break;
         default:
-            std::cerr << "Invalid flag value. Use -f <flag_value=1-6>" << std::endl;
+            std::cerr << "Invalid flag value. Use -f <flag_value=1-4>" << std::endl;
+            std::cerr <<" 1 - Elevator services\n 2 - GPIO Check\n 3 - OLED Check\n 4 - Keypad Check" << std::endl;
+            std::cerr << "Please provide a flag value to run the appropriate service." << std::endl;
             return 1;
     }
 
